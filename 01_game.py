2a557eff169ea761399a1cc0bd06d771b6145159
Ekaterina Divina <ekaterinadivina@gmail.com>
2021-01-05 18:35:45 +0300

    Update 01_game.py

--- 01_game.py
+++ 01_game.py
@@ -1,14 +1,14 @@
 import pygame
-from time import sleep
+
 
 pygame.init()
 
-gamedisplay = pygame.display.set_mode((800, 600) )
+gamedisplay = pygame.display.set_mode((800, 600) ) #Создаём экран размером 800 на 600
 
-gamedisplay.fill((255, 255, 255))
+gamedisplay.fill((255, 255, 255)) #заливаем экран белым цветом
 
-radius = 5
-
+radius = 5  #стартовый радиус окружности
+color = (255, radius, 0)
 game = True  #Продолжается игра или нет
 while game:
 
@@ -18,14 +18,26 @@ while game:
         if e.type == pygame.QUIT:
             game = False
 
-        if e.type == pygame.MOUSEMOTION:
-            pygame.draw.circle(gamedisplay, (255, 0, 0), e.pos, radius)
 
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_UP:
+
+        if e.type == pygame.MOUSEBUTTONDOWN:
+
+            if e.button == 4: #Кручу наверх
                 radius+=1
-            if e.key == pygame.K_DOWN:
+            if e.button == 5: #Кручу вниз
                 radius-=1
+
+            if e.button == pygame.BUTTON_RIGHT:
+                print(1234567890)
+
+
+
+    #Если левая кнопка мыши нажата
+    if pygame.mouse.get_pressed()[0]:
+        mousepose = pygame.mouse.get_pos()  #Запрашиваю позицию мыши
+
+        pygame.draw.circle(gamedisplay, color, mousepose, radius)
+
 
 
     pygame.display.update()
